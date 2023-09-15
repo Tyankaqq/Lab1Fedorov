315b53f4191a3939badfc6d8cd8ad9e43af538a1
Tyankaqq <144240127+Tyankaqq@users.noreply.github.com>
2023-09-15 10:15:25 +0200

    Update Program.cs

--- Lab2Fedorov/Program.cs
+++ Lab2Fedorov/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("введите x:");
+//Высокий уровень 21 варинат
+Console.Write("введите x:");
 double x = double.Parse(Console.ReadLine());
 double sh_x = (Math.Exp(x) - Math.Exp(-x)) / 2;
 double y = sh_x * Math.Tan(x + 1) - Math.Pow(Math.Tan(2 + Math.Sinh(x - 1)), 2);
