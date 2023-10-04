2afcb1b8fcca723e5b1fd3c2820b838872bff1b8
Пользователь <@DESKTOP-VBHFI92>
2023-10-04 14:02:41 +0200

    алеале

---
+++ BANKOMAT/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите номер дня:");
+int n = int.Parse(Console.ReadLine());
+
+
+int m = (n % 7); 
+if (n >= 1 && n <= 31) Console.WriteLine("Январь");
+else if (n >= 32 && n <= 58) Console.WriteLine("Февраль");
+else if (n >= 59 && n <= 89) Console.WriteLine("Март");
+else if (n >= 90 && n <= 119) Console.WriteLine("Апрель");
+else if (n >= 120 && n <= 150) Console.WriteLine("Май");
+else if (n >= 151 && n <= 180) Console.WriteLine("Июнь");
+else if (n >= 181 && n <= 211) Console.WriteLine("Июль");
+else if (n >= 212 && n <= 242) Console.WriteLine("Август");
+else if (n >= 243 && n <= 272) Console.WriteLine("Сентябрь");
+else if (n >= 273 && n <= 303) Console.WriteLine("Октябрь");
+else if (n >= 304 && n <= 333) Console.WriteLine("Ноябрь");
+else if (n >= 334 && n <= 365) Console.WriteLine("Декабрь");
+
+
+switch (m)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+
+    default : Console.WriteLine("Такой масти нет"); break;
+}
---
+++ lab5Fedorov/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите c:");
+double c = double.Parse(Console.ReadLine());
+
+
+if (a != 0)
+{
+    double d = b * b - 4 * a * c;
+    if (d > 0)
+    {
+        double x1 = (-b + Math.Sqrt(d)) / 2 * a;
+        double x2 = (-b + Math.Sqrt(d)) / 2 * a;
+        Console.WriteLine($"x1 = {x1:F2} x2 = {x2:F2}");
+    }
+    else if (d == 0)
+    {
+        double x = -b / (2 * a);
+        Console.WriteLine($"x = {x:F2}");
+    }
+    else Console.WriteLine("Нет корней");
+}
+else Console.WriteLine("Линейное уравнение");
+
+
