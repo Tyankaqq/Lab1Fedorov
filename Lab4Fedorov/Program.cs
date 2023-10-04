c52748010994311064af0e55bd38711d1447ba2f
Пользователь <@DESKTOP-VBHFI92>
2023-10-04 14:42:07 +0200

    lala

---
+++ Lab4Fedorov/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+
+double R = (x * x + y * y );
+if (x >= -1 && x <= 1 && y >= 0 && y <= 1 && R >= 1) Console.WriteLine("Входит");
+
+else Console.WriteLine("Не входит");
