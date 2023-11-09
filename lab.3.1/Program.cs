d5408d2e0bed007e655c56e0bc44c8ad06c9b7be
Пользователь <@DESKTOP-VBHFI92>
2023-11-09 12:44:21 +0200

    lab 3.1

---
+++ lab.3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//средний уровень 21 задание
+try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    Console.Write("Введите x:");
+    int x = int.Parse(Console.ReadLine());
+    double P = 1;
+    double S = 0;
+    
+    for (int t = 2; t <= k; t++)
+    {
+        if (t != 3) P = t * Math.Pow(x, t) / t - 3;
+        for (int i = 1; i <= t; i++)
+        {
+            if (i != 7) S += (i - 2) / (i - 7);
+        }
+    }
+    Console.WriteLine($"P={P * S:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+
+
