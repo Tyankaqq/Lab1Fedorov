c7af45e852ec77029effd306708ae272c6aa3e4c
Your Name <lev.fedorov87@gmail.com>
2023-11-13 23:38:07 +0200

    папапа

---
+++ Lab 4.2/Program.cs
@@ -0,0 +1,11 @@
+﻿//задание 23 выскоий уровень
+int[,] mas = new int[5, 5];
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i == 2 || j == 2) mas[i, j] = 1;
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
