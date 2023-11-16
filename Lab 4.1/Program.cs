f856f989578f41e370cda9e6f1e6d2db82bcc7a9
Your Name <lev.fedorov87@gmail.com>
2023-11-16 23:29:55 +0200

    выфвыфвыф

---
+++ Lab 4.1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Высокий уровень 23 вариант
+Console.Write("Введите размер массива:");
+int n = int.Parse(Console.ReadLine());
+int[] mas = new int[n];
+Random rnd = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+
+    mas[i] = rnd.Next(10, 100);
+    Console.WriteLine(Convert.ToString(mas[i], 2) + " ");
+}
+int max = mas[0];
+int min = mas[0];
+for (int i = 1; i < mas.Length; i++)
+{
+
+    if (mas[i] < min)
+    {
+        min = mas[i];
+    }
+    if (mas[i] > max)
+    {
+        max = mas[i];
+    }
+}
+
+int count = 0;
+Console.WriteLine();
+Console.WriteLine("min: " + min);
+Console.WriteLine("max: " + max);
+
+for (int i = 0; i < mas.Length; i++)
+{
+
+    if ((mas[i] > min) && (mas[i] < max)) count++;
+}
+
+Console.WriteLine("count: " + count);
