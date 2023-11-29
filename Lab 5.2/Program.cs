802ab9c669e0978b25fbfeb19d740df04e37d90e
Your Name <lev.fedorov87@gmail.com>
2023-11-29 19:35:05 +0200

    рррррр

---
+++ Lab 5.2/Program.cs
@@ -0,0 +1,42 @@
+﻿//высокий уровень 25 вариант 
+        string word1 = "apple";
+        string word2 = "banana";
+        string word3 = "cherry";
+
+        int[] letterCount = new int[26];
+
+        
+        foreach (char letter in word1.ToLower())
+        {
+            if (char.IsLetter(letter))
+            {
+                letterCount[letter - 'a']++;
+            }
+        }
+
+        
+        foreach (char letter in word2.ToLower())
+        {
+            if (char.IsLetter(letter))
+            {
+                letterCount[letter - 'a']++;
+            }
+        }
+
+        
+        foreach (char letter in word3.ToLower())
+        {
+            if (char.IsLetter(letter))
+            {
+                letterCount[letter - 'a']++;
+            }
+        }
+
+        Console.WriteLine("Неповторяющиеся буквы: ");
+        for (int i = 0; i < letterCount.Length; i++)
+        {
+            if (letterCount[i] == 1)
+            {
+                Console.Write((char)('a' + i) + " ");
+            }
+        }
