594421bbee7bbae0ade670fd962488fc6db48bcd
Your Name <lev.fedorov87@gmail.com>
2023-12-08 21:01:02 +0200

    hdhdhdhdh

---
+++ lab 6.2/Program.cs
@@ -0,0 +1,9 @@
+﻿using Lab62Fedorov;
+
+double[,] matrix = Class1.InitializeMatrix();
+double minDiagonalElement = Class1.FindMinDiagonalElement(matrix);
+Console.WriteLine($"Минимальный элемент диагонали: {minDiagonalElement}");
+
+double[] vector = Class1.CreateVector(matrix);
+double product = Class1.FindProductOfMaxElementAndIndex(vector);
+Console.WriteLine($"Произведение максимального элемента вектора на его индекс: {product}");
