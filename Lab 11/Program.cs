0d21be9bf51670c33c2f0c8a529fe4494a56a85c
Your Name <lev.fedorov87@gmail.com>
2023-12-25 18:24:25 +0200

    ыывфывфы

---
+++ Lab 11/Program.cs
@@ -0,0 +1,87 @@
+﻿
+
+class Time
+{
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time()
+    {
+    }
+
+    public Time(int hours, int minutes, int seconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int Hours
+    {
+        get => hours;
+        set => hours = value;
+    }
+
+    public int Minutes
+    {
+        get => minutes;
+        set => minutes = value;
+    }
+
+    public int Seconds
+    {
+        get => seconds;
+        set => seconds = value;
+    }
+
+    public int TotalSeconds()
+    {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public void AddSeconds(int secondsToAdd)
+    {
+        int totalSeconds = TotalSeconds() + secondsToAdd;
+
+        hours = totalSeconds / 3600;
+        minutes = (totalSeconds % 3600) / 60;
+        seconds = totalSeconds % 60;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Часы: {hours}, Минуты: {minutes}, Секунды: {seconds}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введите часы: ");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите минуты: ");
+            int minutes = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите секунды: ");
+            int seconds = int.Parse(Console.ReadLine());
+
+            Time time = new Time(hours, minutes, seconds);
+            Console.WriteLine();
+            Console.WriteLine($"Количество секунд в указанном времени: {time.TotalSeconds()}");
+            Console.WriteLine();
+
+            Console.WriteLine("Увеличение времени на 5 секунд");
+            time.AddSeconds(5);
+            time.Print();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Lab 6.2 Library/Class1.cs
@@ -0,0 +1,75 @@
+﻿namespace Lab_6._2_Library
+{
+    public class Matrix
+    {
+        public static double[,] InitializeMatrix()
+        {
+            double[,] matrix = new double[6, 6];
+            
+            Random random = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    matrix[i, j] = random.NextDouble(); 
+                }
+            }
+            return matrix;
+        }
+
+        public static double FindMinDiagonalElement(double[,] matrix)
+        {
+            double minDiagonalElement = double.MaxValue;
+            for (int i = 0; i < 6; i++)
+            {
+                
+                if (matrix[i, i] < minDiagonalElement)
+                {
+                    minDiagonalElement = matrix[i, i];
+                }
+                
+                if (matrix[i, 5 - i] < minDiagonalElement)
+                {
+                    minDiagonalElement = matrix[i, 5 - i];
+                }
+            }
+            return minDiagonalElement;
+        }
+
+        public static double[] CreateVector(double[,] matrix)
+        {
+            double[] vector = new double[6];
+            for (int i = 0; i < 6; i++)
+            {
+                double maxElement = double.MinValue;
+                for (int j = 0; j < 6; j++)
+                {
+                    if (matrix[i, j] > maxElement)
+                    {
+                        maxElement = matrix[i, j];
+                    }
+                }
+                vector[i] = maxElement;
+            }
+            return vector;
+        }
+
+        public static double CreateMatrix(double[] vector)
+        {
+            double maxElement = double.MinValue;
+            int maxIndex = -1;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] > maxElement)
+                {
+                    maxElement = vector[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxElement * maxIndex;
+        }
+    }
+}
+
